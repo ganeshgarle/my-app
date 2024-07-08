68b7a1f1afc32e349424d6a19c4aff67ca3fa28a
Ganesh <garleganesh.c@gmail.com>
2024-07-08 21:05:36 +0530

    Added static list with map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import data from "./data";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState(data);
+  const [inputForm, setInputForm] = useState({
+    name: "",
+    age: "",
+    address: "",
+  });
+
+  const addInput = () => {
+    const { name, age, address } = inputForm;
+    setUsers((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        name: name,
+        age: age,
+        address: address,
+        completed: false,
+      },
+    ]);
+    setInputForm({
+      name: "",
+      age: "",
+      address: "",
+    });
+  };
+
+  const handleCheckBox = (isChecked, id) => {
+    // console.log(isChecked, id);
+    setUsers((prevState) =>
+      prevState.map((user) =>
+        user.id === id ? { ...user, completed: isChecked } : user
+      )
+    );
+  };
+
+  // const deleteAll = () => {
+  //   setUsers([]);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <div className="center"> */}
+      <div>
+        <input
+          value={inputForm.name}
+          onChange={(e) => setInputForm({ ...inputForm, name: e.target.value })}
+          placeholder="Enter name"
+        />
+        <input
+          value={inputForm.age}
+          type="number"
+          onChange={(e) => setInputForm({ ...inputForm, age: e.target.value })}
+          placeholder="Enter Age"
+        />
+        <input
+          value={inputForm.address}
+          onChange={(e) =>
+            setInputForm({ ...inputForm, address: e.target.value })
+          }
+          placeholder="Enter Address"
+        />
+      </div>
+      <div>
+        <button onClick={addInput}>Add</button>
+        {/* {users.length !== 5 && <button onClick={deleteAll}>Delete All</button>} */}
+      </div>
+      {/* </div> */}
+      <div className="list-items">
+        {users.map((user) => {
+          return (
+            <div key={user.id} className="items">
+              <input
+                type="checkbox"
+                checked={user.completed}
+                onChange={(e) => handleCheckBox(e.target.checked, user.id)}
+              />
+              <p>Name - {user.name}</p>
+              <p>Age - {user.age}</p>
+              <p>address - {user.address}</p>
+
+              <button>Edit</button>
+              {user.completed && <button>Delete</button>}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: 1,
+    name: "Jhon Doe",
+    age: 25,
+    address: "USA",
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "Amole D",
+    age: 35,
+    address: "Pune",
+    completed: false,
+  },
+  {
+    id: 3,
+    name: "Nitin Doe",
+    age: 65,
+    address: "Mumbai",
+    completed: false,
+  },
+  {
+    id: 4,
+    name: "Rajesh Doe",
+    age: 55,
+    address: "Pune",
+    completed: false,
+  },
+];
+export default data;
